d78b575cd26b41220bead7e11d4ddc90a5ac858c
Borislav Vezirov <bobi.vez@gmail.com>
2021-08-17 20:30:10 +0300

    added new task

---
+++ Syntax_Functions_and_Statements/01_Fruit.js
@@ -0,0 +1,6 @@
+function solve(fruit, weight, price){
+    weight /= 1000;
+    let money = weight * price;
+    return `I need $${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`
+}
+console.log(solve('orange', 2500, 1.80));
