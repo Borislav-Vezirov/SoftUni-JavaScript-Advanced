9af3417f19cc275c7646123751d109f211be40da
Borislav Vezirov <bobi.vez@gmail.com>
2021-09-02 20:41:00 +0300

    Added new task

---
+++ 03.Objects & Composition-Exercise/07.Towns_to_JSON.js
@@ -0,0 +1,27 @@
+function solve(arr) {
+    let titles = extractElements(arr.shift());
+    let row = arr.
+    map(x => extractElements(x).reduce((acc, el, i) => {
+        acc[titles[i]] = el;
+        return acc
+    }, {}))
+    
+    return JSON.stringify(row);
+
+    function extractElements(str) {
+        return str. 
+        split('|').
+        map(x => x.trim()).
+        filter(x => x !== '').
+        map(x => isNaN(Number(x)) ? x : Number(Number(x).toFixed(2)))
+    }
+}
+console.log(solve(['| Town | Latitude | Longitude |',
+    '| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |'
+]));
+
+console.log((solve(['| Town | Latitude | Longitude |',
+'| Veliko Turnovo | 43.0757 | 25.6172 |',
+'| Monatevideo | 34.50 | 56.11 |']
+)));
---
+++ 03.Objects & Composition-Exercise/08.Rectangle.js
@@ -0,0 +1,12 @@
+function solve(arr) {
+    
+}
+console.log(solve(['| Town | Latitude | Longitude |',
+    '| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |'
+]));
+
+console.log((solve(['| Town | Latitude | Longitude |',
+'| Veliko Turnovo | 43.0757 | 25.6172 |',
+'| Monatevideo | 34.50 | 56.11 |']
+)));
