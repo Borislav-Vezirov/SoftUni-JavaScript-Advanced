f7e9e317e2dde098ee09f1398fd32929563095f8
Borislav Vezirov <bobi.vez@gmail.com>
2021-09-04 19:46:56 +0300

    Upload a few tasks

--- 04.DOM_Introduction_Exercise/05. Table - Search Engine/solution.js
+++ 04.DOM_Introduction_Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,19 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let rowElements = Array.from(document.querySelectorAll('tbody tr'));
+      let searchText  = document.querySelector('#searchField');
 
+      for (let el of rowElements) {
+         el.className = '';
+      }
+
+      rowElements.filter(x => {
+         let cells = Array.from(x.children);
+         if(cells.find(x => x.textContent.includes(searchText.value))){
+            x.className = 'select'
+         }
+      })
+      searchText.value = '';
    }
 }
--- 04.DOM_Introduction_Exercise/06. Format the Text/solution.js
+++ 04.DOM_Introduction_Exercise/06. Format the Text/solution.js
@@ -1,3 +1,8 @@
 function solve() {
-  //TODO
+    let content = document.querySelector('#input')
+    let splitted = content.value.split('.').map(x => x + '.').filter(x => x.length > 1);
+    while(splitted.length > 0){
+      let pararaph = splitted.splice(0, 3);
+      document.getElementById('output').innerHTML += `<p>${pararaph.join('')}</p>`
+    }
 }
