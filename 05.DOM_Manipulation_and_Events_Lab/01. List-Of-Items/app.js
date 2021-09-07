c84a8ef3af228ec6bbfded9a262e5f48de711ed7
Borislav Vezirov <bobi.vez@gmail.com>
2021-09-07 18:48:18 +0300

    Added new task

--- 05.DOM_Manipulation_and_Events_Lab/01. List-Of-Items/app.js
+++ 05.DOM_Manipulation_and_Events_Lab/01. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputElement = document.querySelector('#newItemText');
+    let inputText = inputElement.value;
+    let list = document.querySelector('ul');
+
+    let createdeElementli = document.createElement('li');
+    createdeElementli.textContent = inputText;
+    list.appendChild(createdeElementli);
+    inputElement.value = '';
 }
