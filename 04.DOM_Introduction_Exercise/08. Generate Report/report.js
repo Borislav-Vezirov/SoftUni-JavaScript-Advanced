4eb38b3b0ee160f03c54704d963047d6fd09b02e
Borislav Vezirov <bobi.vez@gmail.com>
2021-09-07 18:38:00 +0300

    Added 2 folder - DOM - Lab & Exersice

--- 04.DOM_Introduction_Exercise/08. Generate Report/report.js
+++ 04.DOM_Introduction_Exercise/08. Generate Report/report.js
@@ -1,3 +1,11 @@
 function generateReport() {
-    //TODO
+    let checkbox = Array.from(document.querySelectorAll('input'));
+    let result = [];
+    for (const checkedEl of checkbox) {
+        let obj = {};
+        if(checkedEl.checked === 'true'){
+
+        }
+    }
+
 }
