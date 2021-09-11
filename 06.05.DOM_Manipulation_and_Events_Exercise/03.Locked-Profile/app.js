dc48fadc37826510bbd1ad1a5df1b96864c726e0
Borislav Vezirov <bobi.vez@gmail.com>
2021-09-11 08:26:14 +0300

    another task added to the repository

--- 06.05.DOM_Manipulation_and_Events_Exercise/03.Locked-Profile/app.js
+++ 06.05.DOM_Manipulation_and_Events_Exercise/03.Locked-Profile/app.js
@@ -1,3 +1,17 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll('button'));
+    
+    for (const button of buttons) {
+        button.addEventListener('click', (e) => {
+            let parentElement  = e.target.parentElement;
+            let getRadioButton = parentElement.querySelector('input[type=radio]:checked');
+            if(getRadioButton.value === 'unlock'){
+                let hiddenField = parentElement.querySelector('div');
+                hiddenField.style.display = hiddenField.style.display === 'block' 
+                ? 'none' 
+                : 'block' 
+                button.textContent = button.textContent === 'Show more' ? 'Hide it' : 'Show more'
+            }
+        })
+    }
 }
