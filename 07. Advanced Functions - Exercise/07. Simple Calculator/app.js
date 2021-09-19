401fe7144a0660bfe800a087dab851e15fc5295c
Borislav Vezirov <bobi.vez@gmail.com>
2021-09-19 20:22:42 +0300

    The new task is on the way..

--- 07. Advanced Functions - Exercise/07. Simple Calculator/app.js
+++ 07. Advanced Functions - Exercise/07. Simple Calculator/app.js
@@ -1,3 +1,20 @@
 function calculator() {
-    // TODO:
+    return  {
+        init: (selector1, selector2, resultSelector) => {
+            selector1      = document.querySelector('#num1').value;
+            selector2      = document.querySelector('#num2').value;
+            resultSelector = document.querySelector('#result').value;
+            console.log(selector1);
+            console.log(selector2);
+
+        },
+        add: () => {
+
+        },
+        subtract:() => {
+
+        }
+    }
 }
+let calc = calculator();
+calc.init();
--- 07. Advanced Functions - Exercise/09. Central Cinema/solution.js
+++ 07. Advanced Functions - Exercise/09. Central Cinema/solution.js
@@ -1,3 +1,68 @@
 function solve() {
-    // TODO
+    let inputElements  = Array.from(document.querySelectorAll('#container input'))
+    let onScreenButton = document.querySelector('#container button');
+    let ulElement      = document.querySelector('#movies ul');
+    
+    onScreenButton.addEventListener('click', addMovieOnScreen);
+
+    function addMovieOnScreen(e){
+        e.preventDefault();
+        
+        let name  = inputElements[0].value;
+        let hall  = inputElements[1].value;
+        let price = inputElements[2].value;
+        
+        if(name !== '' && hall !== '' && price !== '' && !isNaN((Number(price)))){
+            
+            let createLiElement     = document.createElement('li');
+            let createSpanElement   = document.createElement('span');
+            let createStrongElement = document.createElement('strong');
+            let createDivElement    = document.createElement('div');
+            let createSecondStrongElement = document.createElement('strong');
+            let createInputElement  = document.createElement('input');
+            createInputElement.placeholder = 'Tickets Sold';
+            let createButtonElement = document.createElement('button');
+
+            price = Number(price).toFixed(2);
+
+            createSecondStrongElement.textContent = price;
+            createStrongElement.textContent       = `Hall: ${hall}`;
+            createSpanElement.textContent         = name;
+
+            createButtonElement.textContent       = 'Archive';
+            createButtonElement.addEventListener('click', archiveMovie);
+
+            createDivElement.appendChild(createSecondStrongElement);
+            createDivElement.appendChild(createInputElement);
+            createDivElement.appendChild(createButtonElement);
+
+            createLiElement.appendChild(createSpanElement);
+            createLiElement.appendChild(createStrongElement);
+            createLiElement.appendChild(createDivElement);
+
+            ulElement.appendChild(createLiElement);
+        }
+
+        inputElements.map(x => x.value = '');
+    }
+
+    function archiveMovie(e){
+        let liElement     = e.target.parentElement.parentElement;
+        let inputElement  = liElement.querySelector('div input')
+        let inputNumber   = inputElement.value;
+        let archiveUl     = document.querySelector('#archive ul');
+
+        if(inputElement.value !== '' && !isNaN(Number(inputNumber))){
+            let spanElement   = liElement.querySelector('span');
+            let strongElement = liElement.querySelector('div strong');
+            strongElement.textContent = `Total amount: ${(strongElement.value * inputNumber).toFixed(2)}` 
+            let deleteButton = document.createElement('button');
+            deleteButton.textContent = "Delete";
+
+            liElement.appendChild(deleteButton);
+            archiveUl.appendChild(liElement);
+        }
+
+        
+    }
 }
