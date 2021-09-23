c81c8dbd67abe9ec6b1ea07d9a999be8f3ed3709
Borislav Vezirov <bobi.vez@gmail.com>
2021-09-23 07:47:21 +0300

    New task added

---
+++ test/04.Math Enforcer.test.js
@@ -0,0 +1,46 @@
+const mathEnforcer = require('../08.Unit Testing and Error Handling - Exercise/04.Math Enforcer');
+const { assert }   = require('chai');
+
+describe('mathEnforcer', () => {
+    describe('addFive', () => {
+        it('AddFive method should return undefined or add 5 to number', () => {
+
+            assert.strictEqual(mathEnforcer.addFive('str'), undefined);
+            assert.strictEqual(mathEnforcer.addFive(undefined), undefined);
+            assert.strictEqual(mathEnforcer.addFive(null), undefined);
+    
+            assert.strictEqual(mathEnforcer.addFive(5), 10);
+            assert.strictEqual(mathEnforcer.addFive(-3), 2);
+            assert.strictEqual(mathEnforcer.addFive(-7), -2);
+        })
+    })
+
+    describe('subtractTen', () => {
+        it('subtractTen method should return undefined or subtract 10 from the number', () => {
+
+            assert.strictEqual(mathEnforcer.subtractTen('str'), undefined);
+            assert.strictEqual(mathEnforcer.subtractTen(undefined), undefined);
+            assert.strictEqual(mathEnforcer.subtractTen(null), undefined);
+    
+            assert.strictEqual(mathEnforcer.subtractTen(15), 5);
+            assert.strictEqual(mathEnforcer.subtractTen(30), 20);
+            assert.strictEqual(mathEnforcer.subtractTen(-30), -40);
+        })
+    })
+
+    describe('sum', () => {
+        it('sum method should return undefined or the sum of parameters', () => {
+
+            assert.strictEqual(mathEnforcer.sum('str', 1), undefined);
+            assert.strictEqual(mathEnforcer.sum(1, 'str'), undefined);
+            assert.strictEqual(mathEnforcer.sum(null, undefined), undefined);
+            assert.strictEqual(mathEnforcer.sum('str', 'some'), undefined);
+    
+            assert.strictEqual(mathEnforcer.sum(5.5, 5.5), 11);
+            assert.strictEqual(mathEnforcer.sum(50, 10), 60);
+            assert.strictEqual(mathEnforcer.sum(-5, -5), -10);
+            assert.closeTo(mathEnforcer.sum(1.5, 1), 0.01, 2.5);
+      
+        })
+    })
+})
