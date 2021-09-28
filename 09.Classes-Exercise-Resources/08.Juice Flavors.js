c159e54ec66580185b0d48cf9ed10f27f3d515eb
Borislav Vezirov <bobi.vez@gmail.com>
2021-09-28 09:52:49 +0300

    One more task added

--- 09.Classes-Exercise-Resources/08.Juice Flavors.js
+++ 09.Classes-Exercise-Resources/08.Juice Flavors.js
@@ -1,9 +1,48 @@
 function solve(arr) {
     
+    let bottles = [];
+    let fruitCollection = {};
+
+    for (let el of arr) {
+        let [fruit, qty] = el.split(' => ');
+        qty = Number(qty);
+        if(fruitCollection[fruit] === undefined){
+            fruitCollection[fruit] = qty;
+        }else{
+            fruitCollection[fruit] += qty;
+        }
+
+        if(fruitCollection[fruit] > 1000){
+            let bottle = Math.trunc(fruitCollection[fruit] / 1000);
+            let float  = fruitCollection[fruit] % 1000;
+    
+            let isInArray = false;
+            bottles.map(x => {
+                if(x[fruit] !== undefined){
+                    x[fruit] += bottle;
+                    isInArray = true;
+                }
+            })
+            
+            if(!isInArray){
+                bottles.push({[fruit]: bottle});
+            }
+
+            if(float !== undefined){
+                fruitCollection[fruit] = float;
+            }else{
+                fruitCollection[fruit] = 0
+            }
+        }
+    }
+    let result = '';
+    bottles.map(x => Object.keys(x).forEach(el => result += `${el} => ${x[el]}\n`));
+    return result
 }
-console.log(solve(['Orange => 2000',
-'Peach => 1432',
-'Banana => 450',
-'Peach => 600',
-'Strawberry => 549']
+console.log(solve(['Kiwi => 234',
+'Pear => 2345',
+'Watermelon => 3456',
+'Kiwi => 4567',
+'Pear => 5678',
+'Watermelon => 6789']
 ));
