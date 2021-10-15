00beea0111ea748ddb3378d555c03b19810e224b
Borislav Vezirov <bobi.vez@gmail.com>
2021-10-15 09:33:53 +0300

    edit second task in Prototype and Inheritance

--- Prototype and Inheritance/02.String Extension.js
+++ Prototype and Inheritance/02.String Extension.js
@@ -1,30 +1,73 @@
-(function solve() {
-
-    String.prototype.ensureStart = function (param) {
-        let start = this.startsWith(param);
-
-        return start ? this : param + this;
+(function () {
+    String.prototype.ensureStart = function (str) {
+        if (this.slice(0, str.length) === str) {
+            return this.toString();
+        }
+ 
+        return `${str}${this}`
+    };
+ 
+    String.prototype.ensureEnd = function (str) {
+        if (this.endsWith(str)) {
+            return this.toString();
+        }
+ 
+        return `${this}${str}`;
     }
-
-    String.prototype.ensureEnd = function (param) {
-        let end = this.endsWith(param);
-
-        return start ? this : param + this;
+ 
+    String.prototype.isEmpty = function () {
+        return this.toString() === '';
     }
-
-})();
+ 
+    String.prototype.truncate = function (n) {
+        if (this.length <= n) {
+            return this.toString()
+        }
+ 
+        if (this.includes(' ')) {
+ 
+            let words = this.split(' ');
+            words.pop()
+            while (words.join(' ').length + 3 > n){
+                words.pop()
+            }
+ 
+ 
+            let sentance = `${words.join(' ')}...`;
+            return sentance;
+        }
+ 
+        if(n > 3) {
+            let string = `${this.slice(0, n - 3)}...`
+            return string
+        }
+ 
+ 
+        return '.'.repeat(n)
+    }
+ 
+    String.format = function(string, ...params) {
+        let repalceRegex = /{(\d+)}/g;
+        let replaced = string.replace(repalceRegex, (match, group1) => {
+            let index = Number(group1)
+            if(params[index] !== undefined){
+                return params[index];
+            }
+            return match;
+        })
+        return replaced;
+    }
+}())
 
 let str = 'my string';
 str = str.ensureStart('my');
 str = str.ensureStart('hello ');
-str = str.ensureEnd('ing');
-console.log(str);
-// str = str.truncate(16);
-// str = str.truncate(14);
-// str = str.truncate(8);
-// str = str.truncate(4);
-// str = str.truncate(2);
-// str = String.format('The {0} {1} fox',
-//   'quick', 'brown');
-// str = String.format('jumps {0} {1}',
-//   'dog');
+str = str.truncate(16);
+str = str.truncate(14);
+str = str.truncate(8);
+str = str.truncate(4);
+str = str.truncate(2);
+str = String.format('The {0} {1} fox',
+  'quick', 'brown');
+str = String.format('jumps {0} {1}',
+  'dog');
